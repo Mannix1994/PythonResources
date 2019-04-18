698e9762c777e136c74124f1ef5caf36e8c2ca2e
wang <wangmingzhang@outlook.com>
2019-04-18 14:01:56 +0800

    添加更多的类类

--- caffe/__init__.pyi
+++ caffe/__init__.pyi
@@ -1,8 +1,10 @@
-from typing import Tuple, Optional, Union, overload
+from typing import Tuple, Optional, Union, overload, Any, Callable
 
 from . import io
-from .pycaffe import Net, SGDSolver, NesterovSolver, AdaDeltaSolver
-from .pycaffe import RMSPropSolver, AdaDeltaSolver, NCCL, Timer
+from .pycaffe import _Net_backward, _Net_forward, _Net_forward_all
+from .pycaffe import _Net_forward_backward_all, _Net_set_input_arrays
+from .pycaffe import _Net_blobs, _Net_blob_loss_weights, _Net_layer_dict
+from .pycaffe import _Net_params, _Net_batch, _Net_inputs, _Net_outputs, _Net_get_id_name
 from .proto.caffe_pb2 import TRAIN, TEST
 from .classifier import Classifier
 from .detector import Detector
@@ -39,4 +41,207 @@ def solver_count() -> int: ...
 
 def solver_rank() -> int: ...
 
-# ..........................................
+# .........................................
+
+# -------------help(caffe.Net)-------------
+class Net:
+    def __init__(self, network_file: str, phase: Optional[int]=TRAIN, level: Optional[int]=0, stages: Optional[str]=None , weights: Optional[str]=None): ...
+
+    @overload
+    def __init__(self, network_file: str, weights: str,  phase: Optional[int]=TRAIN): ...
+
+
+    def after_backward(self, callback: Optional[Callable]) -> None : ...
+
+    def after_forward(self, callback: Callable) -> None : ...
+
+    backward = _Net_backward(self, diffs=None, start=None, end=None, **kwargs)
+    """
+    Backward pass: prepare diffs and run the net backward.
+      
+      Parameters
+        ----------
+        diffs : list of diffs to return in addition to bottom diffs.
+        kwargs : Keys are output blob names and values are diff ndarrays.
+                If None, top diffs are taken from forward loss.
+        start : optional name of layer at which to begin the backward pass
+        end : optional name of layer at which to finish the backward pass
+            (inclusive)
+        
+        Returns
+        -------
+        outs: {blob name: diff ndarray} dict.
+    """
+
+    def before_backward(self, callback: Callable) -> None : ...
+
+    def before_forward(self, callback: Callable) -> None : ...
+
+    def clear_param_diffs(self) -> None : ...
+
+    def copy_from(self, net: str, ) -> None : ...
+
+    forward = _Net_forward(self, blobs=None, start=None, end=None, **kwargs)
+    """
+    Forward pass: prepare inputs and run the net forward.
+
+        Parameters
+        ----------
+        blobs : list of blobs to return in addition to output blobs.
+        kwargs : Keys are input blob names and values are blob ndarrays.
+                 For formatting inputs for Caffe, see Net.preprocess().
+                 If None, input is taken from data layers.
+        start : optional name of layer at which to begin the forward pass
+        end : optional name of layer at which to finish the forward pass
+              (inclusive)
+    
+        Returns
+        -------
+        outs : {blob name: blob ndarray} dict.
+    """
+
+    forward_all = _Net_forward_all(self, blobs=None, **kwargs)
+    """
+    Run net forward in batches.
+
+        Parameters
+        ----------
+        blobs : list of blobs to extract as in forward()
+        kwargs : Keys are input blob names and values are blob ndarrays.
+                 Refer to forward().
+    
+        Returns
+        -------
+        all_outs : {blob name: list of blobs} dict.
+    """
+
+    forward_backward_all = _Net_forward_backward_all(self, blobs=None, diffs=None, **kwargs)
+    """
+    Run net forward + backward in batches.
+
+        Parameters
+        ----------
+        blobs: list of blobs to extract as in forward()
+        diffs: list of diffs to extract as in backward()
+        kwargs: Keys are input (for forward) and output (for backward) blob names
+                and values are ndarrays. Refer to forward() and backward().
+                Prefilled variants are called for lack of input or output blobs.
+    
+        Returns
+        -------
+        all_blobs: {blob name: blob ndarray} dict.
+        all_diffs: {blob name: diff ndarray} dict.
+    """
+
+    def load_hdf5(self, filename: str) -> None : ...
+
+
+    def reshape(self) -> None : ...
+    """
+    @brief Reshape all layers from bottom to top.
+    
+    This is useful to propagate changes to layer sizes without running
+    a forward pass, e.g. to compute output feature size.
+    """
+
+
+    def save(self, filename: str) -> None : ...
+
+    def save_hdf5(self, filename: str) -> None : ...
+
+
+    set_input_arrays = _Net_set_input_arrays(self, data, labels)
+    """
+    Set input arrays of the in-memory MemoryDataLayer.
+    (Note: this is only for networks declared with the memory data layer.)
+    """
+
+    def share_with(self, net: Net) -> None : ...
+
+    # Attach methods to Net.
+    blobs = _Net_blobs
+    blob_loss_weights = _Net_blob_loss_weights
+    layer_dict = _Net_layer_dict
+    params = _Net_params
+    _batch = _Net_batch
+    inputs = _Net_inputs
+    outputs = _Net_outputs
+    top_names = _Net_get_id_name(Net._top_ids, "_top_names")
+    bottom_names = _Net_get_id_name(Net._bottom_ids, "_bottom_names")
+# .........................................
+
+
+# -------------Solver----------------------
+class Solver:
+    def __init__(self, solver_parameter: str) -> None : ...
+    def add_callback(self, arg2, arg3) -> None : ...
+    def apply_update(self) -> None : ...
+    def restore(self, resume_file: str) -> None : ...
+    def share_weights(self, net: Net) -> None : ...
+    def snapshot(self) -> None :...
+    def solve(self, resume_file: str) -> None : ...
+    def step(self, iters: int) -> None : ...
+    @property
+    def iter(self) -> int: ...
+    @property
+    def net(self)-> Net: ...
+    @property
+    def param(self)-> Any: ...
+    @property
+    def test_nets(self)-> Net: ...
+# .........................................
+
+# -------------help(caffe.SGDSolver)-------
+class SGDSolver(Solver):
+    @property
+    def lr(self) -> float: ...
+# .........................................
+
+
+# -------help(caffe.NesterovSolver)-------
+class NesterovSolver(SGDSolver):
+    @property
+    def lr(self) -> float: ...
+# .........................................
+
+# -------help(caffe.AdaGradSolver)--------
+class AdaGradSolver(SGDSolver):
+    @property
+    def lr(self) -> float: ...
+# .........................................
+
+
+# -------help(caffe.RMSPropSolver)--------
+class RMSPropSolver(SGDSolver):
+    @property
+    def lr(self) -> float: ...
+# .........................................
+
+# -------help(caffe.AdaDeltaSolver)--------
+class AdaDeltaSolver(SGDSolver):
+    @property
+    def lr(self) -> float: ...
+# .........................................
+
+
+# -------help(caffe.AdamSolver)--------
+class AdamSolver(SGDSolver):
+    @property
+    def lr(self) -> float: ...
+# .........................................
+
+
+# -------help(caffe.NCCL)--------
+class NCCL:
+    def __init__(self, solver: Solver, uid: str): ...
+# .........................................
+
+
+# -------help(caffe.Timer)--------
+class Timer:
+    def __init__(self): ...
+    def start(self) -> None: ...
+    def stop(self) -> None: ...
+    @property
+    def ms(self) -> None: ...
+# .........................................
